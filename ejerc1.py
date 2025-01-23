1dc2635d9f61c0e03ba11f75ac975eca1c0a5b31
Paula0802 <leguizamonsuarezp@gmail.com>
2025-01-23 12:06:24 -0500

    realizar README

--- ejerc1.py
+++ ejerc1.py
@@ -1,1 +1,46 @@
-print("Hola")
+import pandas as pd
+import numpy as np
+
+datos= {'Nombre':['Leonardo','Joaquin','Alex','Paula','Enrique', 'Jhon'],
+        'Materias':['Lenguaje','EDF','Química','Matemáticas','Inglés','Historia'],
+        'Calificaciones':['18','20','13','19','16','17'],
+        'Deportes':['Fútbol','Natación','Tenis','Voley','Natación','Balón Mano']
+        }
+df= pd.DataFrame(datos)
+print(df)
+print('\n'*4)
+
+#DATOS NO VÁLIDOS
+datos2= {'Nombre':['Leonardo','Joaquin','Alex','Paula','Enrique', 'Jhon'],
+        'Materias':['Lenguaje','EDF','Química','Matemáticas','Inglés','Historia'],
+        'Calificaciones':['18','20',np.nan,'19','16','17'],
+        'Deportes':['Fútbol','Natación','Tenis','Voley','N/A','Balón Mano']
+        }
+df2=pd.DataFrame(datos2)
+print(df2)
+print('\n'*1)
+print(df2.info())
+
+#ESTADÍSTICAS BÁSICAS
+print(df2.describe())
+print('n'*4)
+nuevo= pd.DataFrame(df2)
+nuevo=nuevo.replace(np.nan,"0")
+print (nuevo)
+#ELIMINAR REGISTRO POR COLUMNAS
+columna= df2[df2['Nombre']!='Joaquin']
+print(columna)
+#CONVERTIR LOS NUMEROS A ENTEROS
+df['Calificaciones']=df.Calificaciones.astype(int)
+print(df.describe())
+
+#ESTADISTICAS INDIVIDUALES
+print("Promedio:", df['Calificaciones'].mean())
+
+
+
+
+
+
+
+
